35f72fcfc793faab3b7cb6a8b13521350164e9c6
Ben Parisot <me@benparisot.com>
2017-12-10 13:06:31 -0800

    style form

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ function Cat (catName, age, personality, color) {
 	this.catName = catName;
 	this.age = age;
 	this.personalityAnswer = personality;
+	this.catColor = color;
+	this.catPhoto = 0;
+	this.catTrait = 0;
 	this.independent = ["Staring out windows", "Hiding in boxes", "Being fed treats", "Meowing at 3AM", "Sitting on the highest shelf of the cat tree", "Rubbing around your ankles and then running away", "Watching you sleep", "Long naps"];
 	this.playful = ["Attacking ankles", "Playing with toy mice", "Chasing laser pointers", "Rolling around on the floor for no reason", "Eating house plants", "Playing fetch", "Eating catnip, and then running around the house"];
 	this.cuddly = ["Sitting on your lap", "Being held like a baby", "Laying on your computer when you are trying to work", "Purring", "Waking you up by putting their paw on your face", "Sleeping under the blankets", "Baking bread", "Good ear scratches"];
 	this.curious = ["Trying to catch birds through windows", "Climbing onto the refridgerator", "Batting breakables off of shelves", "Greeting visitors", "Trying to escape out the front door", "Eating food you left on out on the counter", "Getting lost in walls"];
-	this.catColor = color;
 	this.orange = ["<img src='img/orange/orange01.jpg' width='100%'>", "<img src='img/orange/orange02.jpg' width='100%'>"];
 	this.white = ["<img src='img/white/white01.jpg' width='100%'>", "<img src='img/white/white02.jpg' width='100%'>", "<img src='img/white/white03.jpg' width='100%'>"];
 	this.black = ["<img src='img/black/black01.jpg' width='100%'>", "<img src='img/black/black02.jpg' width='100%'>"];
@@ -15,101 +17,173 @@ function Cat (catName, age, personality, color) {
 	this.grey = ["<img src='img/grey/grey01.jpg' width='100%'>", "<img src='img/grey/grey02.jpg' width='100%'>", "<img src='img/grey/grey03.jpg' width='100%'>"];
 	};
 
+	function catFamily () {
+		this.catName = new Array(1);
+		this.catColor = new Array(1);
+		this.catPersonality = new Array(1);
+		this.catPersonalityType = new Array(1);
+		this.catAge = new Array(1)
+		this.catPhoto = new Array(1)
+		this.numberOfCats = 0;
+	}
+
 Cat.prototype.Random = function() {
   if (this.personalityAnswer === "cuddly") {
-			var cuddleTrait;
-			cuddleTrait = (this.cuddly[Math.floor(Math.random() * this.cuddly.length)]);
-			catFamily.catPersonality.push(cuddleTrait);
-			return (cuddleTrait);
+		 	this.catTrait = (this.cuddly[Math.floor(Math.random() * this.cuddly.length)]);
+			return (this.catTrait);
   } else if (this.personalityAnswer === "curious") {
-			var curiousTrait;
-			curiousTrait = (this.curious[Math.floor(Math.random() * this.curious.length)]);
-			catFamily.catPersonality.push(curiousTrait);
-			return (curiousTrait);
+			this.catTrait = (this.curious[Math.floor(Math.random() * this.curious.length)]);
+			return (this.catTrait);
   } else if (this.personalityAnswer === "playful") {
-			var playfulTrait;
-			playfulTrait = (this.playful[Math.floor(Math.random() * this.playful.length)]);
-			catFamily.catPersonality.push(playfulTrait);
-			return (playfulTrait);
+			this.catTrait = (this.playful[Math.floor(Math.random() * this.playful.length)]);
+			// catFamily.catPersonality.push(playfulTrait);
+			return (this.catTrait);
   } else (this.personalityAnswer === "independent")
-			var independentTrait;
-			independentTrait = (this.independent[Math.floor(Math.random() * this.independent.length)]);
-			catFamily.catPersonality.push(independentTrait);
-			return (independentTrait);
+			this.catTrait = (this.independent[Math.floor(Math.random() * this.independent.length)]);
+			// catFamily.catPersonality.push(independentTrait);
+			return (this.catTrait);
 };
 
 Cat.prototype.randomImg = function() {
   if (this.catColor === "orange") {
-      var orangePic = this.orange[Math.floor(Math.random() * this.orange.length)];
-			catFamily.catPhoto.push(orangePic);
-			return (orangePic);
+      this.catPhoto = this.orange[Math.floor(Math.random() * this.orange.length)];
+			// catFamily.catPhoto.push(orangePic);
+			return (this.catPhoto);
 		} else if (this.catColor === "black") {
-	    var blackPic = this.black[Math.floor(Math.random() * this.black.length)];
-			catFamily.catPhoto.push(blackPic);
-			return (blackPic);
+	    this.catPhoto = this.black[Math.floor(Math.random() * this.black.length)];
+			// catFamily.catPhoto.push(blackPic);
+			return (this.catPhoto);
 		} else if (this.catColor === "white") {
-		  var whitePic = this.white[Math.floor(Math.random() * this.white.length)];
-			catFamily.catPhoto.push(whitePic);
-			return (whitePic);
+		  this.catPhoto = this.white[Math.floor(Math.random() * this.white.length)];
+			// catFamily.catPhoto.push(whitePic);
+			return (this.catPhoto);
 		} else if (this.catColor === "grey") {
-		  var greyPic = this.grey[Math.floor(Math.random() * this.grey.length)];
-			catFamily.catPhoto.push(greyPic);
-			return (greyPic);
+		  this.catPhoto = this.grey[Math.floor(Math.random() * this.grey.length)];
+			// catFamily.catPhoto.push(greyPic);
+			return (this.catPhoto);
 		} else {
-			var brownPic = this.brown[Math.floor(Math.random() * this.brown.length)];
-			catFamily.catPhoto.push(brownPic);
-			return (brownPic);
+			this.catPhoto = this.brown[Math.floor(Math.random() * this.brown.length)];
+			// catFamily.catPhoto.push(brownPic);
+			return (this.catPhoto);
 		}
 };
 
-
-function catFamily () {
-	catName = [];
-	catColor = [];
-	catPersonality = [];
-	catAge = []
-	catPhoto = []
-	numberOfCats = 0;
-}
-
 catFamily.prototype.addtoFamily = function() {
-	this.catName.push(brownPic);
-}
+	if (this.numberOfCats < 1){
+		this.catPhoto.unshift(newCat.catPhoto);
+		this.catName.unshift(newCat.catName);
+		this.catAge.unshift(newCat.age);
+		this.catColor.unshift(newCat.catColor);
+		this.catPersonalityType.unshift(newCat.personalityAnswer);
+		this.catPersonality.unshift(newCat.catTrait);
+		this.catPhoto.pop();
+		this.catName.pop();
+		this.catAge.pop();
+		this.catColor.pop();
+		this.catPersonalityType.pop();
+		this.catPersonality.pop();
+	} else {
+		this.catPhoto.unshift(newCat.catPhoto);
+		this.catName.unshift(newCat.catName);
+		this.catAge.unshift(newCat.age);
+		this.catColor.unshift(newCat.catColor);
+		this.catPersonalityType.unshift(newCat.personalityAnswer);
+		this.catPersonality.unshift(newCat.catTrait);
+	};
+	this.numberOfCats += 1;
+};
+
+catFamily.prototype.showFamily = function() {
+	var currentCatNumber = this.numberOfCats;
+	if (this.numberOfCats >= 1)
+		for (index = 0; index < currentCatNumber ; index += 1){
+			$("#catFamilySectionRight").append("<div class='row'><div class='col-md-3' id='familyPhotoResults'>" + this.catPhoto[index] + "</div><div class='col-md-9' id='familyResults'><p>Name: " + this.catName[index] + "</p><p>Age: " + this.catAge[index] + "</p><p>Personality Type: " + this.catPersonalityType[index] + "</p><p>Favorite Thing: " + this.catPersonality[index] + "</p></div></div>");
+		};
+};
+
+function resetform() {
+	$("#check").prop("checked", true);
+	$("#alsocheck").prop("checked", true);
+	$("#catName").val("");
+	$("#age").val(1);
+	};
+
 
 //user interface logic
 $(document).ready(function(){
+	newcatFamily = new catFamily();
 	$("#addACat").click(function(event){
     event.preventDefault();
+		resetform();
     $("#addACatSection").show();
 		$("#landingSection").hide();
+		$("#catFamilySection").hide();
+		$("#aboutUsSection").hide();
+		$("#catFamilySectionRight").empty();
   });
 	$("#start").click(function(event){
     event.preventDefault();
     $("#addACatSection").show();
 		$("#landingSection").hide();
   });
+	$("#yourFamily").click(function(event){
+    event.preventDefault();
+		if (newcatFamily.numberOfCats >= 1) {
+			$("#catFamilySectionRight").empty();
+			newcatFamily.showFamily();
+	    $("#catFamilySection").show();
+			$("#resultSection").hide();
+			$("#addACatSection").hide();
+			$("#landingSection").hide();
+			$("#aboutUsSection").hide();
+		}
+  });
+	$("#aboutUs").click(function(event){
+		event.preventDefault();
+		$("#catFamilySection").hide();
+		$("#resultSection").hide();
+		$("#addACatSection").hide();
+		$("#landingSection").hide();
+		$("#aboutUsSection").show();
+	});
 	$("#showFormAgain").click(function(event){
     event.preventDefault();
+		resetform();
     $("#addACatSection").show();
 		$("#resultSection").hide();
+		$("#catFamilySectionRight").empty();
   });
 	$("#addtoFamily").click(function(event){
     event.preventDefault();
+		newcatFamily.addtoFamily();
+		newcatFamily.showFamily();
     $("#catFamilySection").show();
 		$("#resultSection").hide();
+		resetform();
   });
+	$("#addAnotherCat").click(function(event){
+    event.preventDefault();
+		resetform();
+    $("#addACatSection").show();
+		$("#resultSection").hide();
+		$("#catFamilySection").hide();
+		$("#catFamilySectionRight").empty();
+  });
+	// $("#goGetThem").click(function(event){s
+  //   event.preventDefault();
+  // });
 	$("form#cat-form").submit(function(event){
     event.preventDefault();
     var personName = $("#name").val();
     var age = $("#age").val();
 		var catColor = $("#color").val();
     var catName = $("#catName").val();
-    var personalityAnswer = $("input:radio[name=personality]:checked").val();
+    var personalityAnswer = $("#personality").val();
 		newCat = new Cat(catName, age, personalityAnswer, catColor);
 		$("#fam-name").text(personName + ", we'd like you to meet:");
 		$("#results").html("<span id='catResults'>" + "<p>Name: " + newCat.catName + "</p>" + "<p>Age: " + newCat.age + "</p>" + "<p>Favorite Thing: " + newCat.Random() + "</p></span>");
 		$("#photoResults").html(newCat.randomImg());
-		$("#name").hide();
+		$("#name-section").hide();
 		$("#resultSection").show();
 		$("#addACatSection").hide();
   });
